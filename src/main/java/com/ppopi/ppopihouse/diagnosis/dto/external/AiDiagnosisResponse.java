aa666b4a69d3cf71ab65951265e753a7521ad1be
10Hyuna <kimhaa1005@naver.com>
2026-06-08 21:49:10 +0900

    fix: 오류 수정 진행 중

--- src/main/java/com/ppopi/ppopihouse/diagnosis/dto/external/AiDiagnosisResponse.java
+++ src/main/java/com/ppopi/ppopihouse/diagnosis/dto/external/AiDiagnosisResponse.java
@@ -1,5 +1,6 @@
 package com.ppopi.ppopihouse.diagnosis.dto.external;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,9 +10,19 @@ public class AiDiagnosisResponse {
 
     private String disease;
     private String triage;
+
+    @JsonAlias({"triage_confidence", "triageConfidence"})
     private float triageConfidence;
+
+    @JsonAlias({"family_label", "familyLabel", "affected_area", "affectedArea"})
     private String familyLabel;
+
+    @JsonAlias({"guidance_message", "guidanceMessage"})
     private String guidanceMessage;
+
+    @JsonAlias({"guidance_action", "guidanceAction"})
     private String guidanceAction;
+
+    @JsonAlias({"guidance_warning", "guidanceWarning"})
     private String guidanceWarning;
 }
